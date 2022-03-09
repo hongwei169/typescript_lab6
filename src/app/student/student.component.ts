779f7e5f08c2d950226bdb8ab51ef30a6a2d82c1
HongWeiPhang <G00388774@gmit.ie>
2022-03-09 10:36:35 +0000

    'lab'

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../Services/data.service';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+  students: any[] = [];
+  weather: any[] = [];
+
+  constructor(private service: DataService) { }
+
+  ngOnInit(): void {
+    this.service.GetStudentData().subscribe(
+      (data) => {
+        this.students = data.students;
+        console.log(this.students);
+      }
+    );
+
+    this.service.GetWeatherData().subscribe(
+      (wdata) => {
+        this.weather = wdata.weather;
+      }
+    );
+  }
+
+}
